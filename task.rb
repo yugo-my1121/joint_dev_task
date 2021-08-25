a3a1a9e06d6e359a3540b0453d3242921f0985f5
yugo-my1121 <mygto1121@docomo.ne.jp>
2021-08-25 22:03:09 +0900

    Q8とQ9提出

--- task.rb
+++ task.rb
@@ -71,7 +71,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -81,7 +82,9 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do |user,i|
+    puts "会員No.#{i} #{user}さん"
+  end
 end
 
 def q10
