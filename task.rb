e78b71c9aa187ecb4d1cd0d55f33e71482fab488
yugo-my1121 <mygto1121@docomo.ne.jp>
2021-08-26 22:14:15 +0900

    Q10とQ11提出

--- task.rb
+++ task.rb
@@ -91,13 +91,27 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
+  foods.each do |food|
+    if food.index("うに")
+      puts "#{food}：好物です"
+    else
+      puts "#{food}:まぁまぁ好きです"
+    end
 
+  end
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
+  answer = sports.flatten.uniq
+
+  answer.each.with_index(1) do |sport,i|
+    puts "No#{i} #{sport}"
+  end
+
+
 
 end
 
